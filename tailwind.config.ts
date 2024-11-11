141bb19c1c70a490679dcd0db2f9534368d8b6b8
GwonYeong <rnjsdud980@naver.com>
2024-11-11 19:46:11 +0900

    feat: 폰트와 svg 사전 설정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,25 @@
 import type { Config } from "tailwindcss";
 
+const colors = {
+  backgroundSecondary: "#F2F2F7",
+  backgroundPrimary: "#FFFFFF",
+  white: "#ffffff",
+  primary: "#0066FF",
+  gray100: "#F2F2F7",
+  gray200: "#E5E5EA",
+  gray300: "#D1D1D6",
+  gray400: "#C7C7CC",
+  gray500: "#AEAEB2",
+  gray600: "#8E8E93",
+  systemRed: "#FF3B30",
+};
+
+const textColors = {
+  textDefault: "#1E1E1E",
+  textSecondary: "#757575",
+  textTertiary: "#B3B3B3",
+};
+
 export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,11 +28,59 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+      colors,
+      textColors,
     },
   },
-  plugins: [],
+  plugins: [
+    ({ addComponents }: { addComponents: any }) => {
+      addComponents({
+        ".text-title-bold": {
+          fontSize: "24px",
+          fontWeight: "700",
+          letterSpacing: "-0.02em",
+        },
+        ".text-title-md": {
+          fontSize: "20px",
+          fontWeight: "600",
+          letterSpacing: "-0.02em",
+        },
+        ".text-body-md": {
+          fontSize: "16px",
+          fontWeight: "500",
+          letterSpacing: "-0.02em",
+        },
+        ".text-body-sm": {
+          fontSize: "16px",
+          fontWeight: "600",
+          letterSpacing: "-0.02em",
+        },
+        ".text-body2-md": {
+          fontSize: "15px",
+          fontWeight: "500",
+          letterSpacing: "-0.02em",
+        },
+        ".text-body3-sm": {
+          fontSize: "15px",
+          fontWeight: "500",
+          letterSpacing: "-0.02em",
+        },
+        ".text-body3-rg": {
+          fontSize: "15px",
+          fontWeight: "400",
+          letterSpacing: "-0.02em",
+        },
+        ".text-caption-sm": {
+          fontSize: "13px",
+          fontWeight: "600",
+          letterSpacing: "-0.02em",
+        },
+        ".text-caption-md": {
+          fontSize: "12px",
+          fontWeight: "500",
+          letterSpacing: "-0.02em",
+        },
+      });
+    },
+  ],
 } satisfies Config;
